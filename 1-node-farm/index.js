75a1e1d674a57f7e8836bfec2633c2063348fd89
unknown <nicole@rocketdaddy.ai>
2024-01-29 13:01:56 -0500

    through lesson 23

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs'); // returns object with usable functions
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////
 // FILES
@@ -31,61 +33,68 @@ const url = require('url');
 
 ////////////////////////////////////
 // SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8'); // NOTE: these can use 'sync' because it's top level code and only executes once
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+); // NOTE: these can use 'sync' because it's top level code and only executes once
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true);
+  const { query, pathname } = url.parse(req.url, true);
 
-    // Overview page
-    if(pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {'Content-type': 'text/html'});
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        res.end(output);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     // Product page
-    } else if (pathname === '/product') {
-        res.writeHead(200, {'Content-type': 'text/html'});
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-        res.end(output);
+    res.end(output);
 
     // API
-    } else if (pathname === '/api') {
-        res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(data);
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
 
     // Not found
-    } else {
-        res.writeHead(404, { // always send BEFORE the response
-            'Content-type': 'text/html',
-            'my-own-header': 'hellow-world'
-        });
-        res.end('<h1>This page could not be found!</h1>');
-    }
+  } else {
+    res.writeHead(404, {
+      // always send BEFORE the response
+      'Content-type': 'text/html',
+      'my-own-header': 'hellow-world',
+    });
+    res.end('<h1>This page could not be found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port 8000');
 });
