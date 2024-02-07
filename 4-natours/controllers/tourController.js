3726cc99564c8036003234eb6a698920cebc72cc
unknown <nicole@rocketdaddy.ai>
2024-02-07 16:26:18 -0500

    through lesson 92

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,93 +1,104 @@
-const fs = require("fs");
+// const fs = require("fs");
+const Tour = require("./../models/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
+exports.getAllTours = async (req, res) => {
+  try {
+    // See mongoose documentation - https://mongoosejs.com/docs/queries.html
+    const tours = await Tour.find();
 
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours, // ES6 syntax allows for only writing 'tours' once since property name and value name match (tours: tours)
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
       status: "fail",
-      message: "Invalid ID",
+      message: err,
     });
   }
-  next();
 };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.getTour = async (req, res) => {
+  try {
+    // See mongoose documentation - https://mongoosejs.com/docs/queries.html
+    const tour = await Tour.findById(req.params.id);
+    // helper function to Tour.findOne({ _id: req.params.id })
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
       status: "fail",
-      message: "Invalid request",
+      message: err,
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // See mongoose documentation - https://mongoosejs.com/docs/queries.html
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    }); // 201 - for 'created'
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent",
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // for optional paramaters -> :y?
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+exports.updateTour = async (req, res) => {
+  try {
+    // See mongoose documentation - https://mongoosejs.com/docs/queries.html
+    // NOTE about patch v put: PATCH only updates specified values, PUT completely replaces original object with the new one
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  console.log(req.body); // possible due to middleware
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body); // Merges two objects into one
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      }); // 201 - for 'created'
-    }
-  );
-
-  //   res.send("Done"); // Always send back something to finish request/response cycle
-};
-
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour here...>",
-    },
-  });
-};
-
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
